0f0a95978d8b59384a51513f865d7b700bf499ca
sayroahm <sakea1221@gmail.com>
2026-03-15 12:50:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/api/data", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ name: "Evan", status: "online" }, null, 2));
-});
-
 app.all("*", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.status(403).send(JSON.stringify({"errors":[{"code":0,"message":""}]}
 , null, 2));
 });
 
-// Listen on Railway port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
