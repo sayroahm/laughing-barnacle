4bf1317c2b450abe227f407edf6b472fb83e27e8
sayroahm <sakea1221@gmail.com>
2026-03-15 13:15:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.all("*", (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.status(420).send(JSON.stringify({ "errors": [ { "code": 0, "message": "" } ] }));
+  res.status(426).send(JSON.stringify({ "errors": [ { "code": 0, "message": "" } ] }));
 });
 
 const PORT = process.env.PORT || 3000;
