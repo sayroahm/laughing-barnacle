f9136b976fbe3394d39f992b9892ea4777023c62
sayroahm <sakea1221@gmail.com>
2026-03-15 12:47:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const app = express();
 
-// Example real endpoint (optional)
 app.get("/api/data", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ name: "Evan", status: "online" }, null, 2));
 });
 
-// Catch-all for index and any nonexistent route
 app.all("*", (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.status(200).send(JSON.stringify({
-    errors: [
-      {
-        code: 0,
-        message: ""
-      }
-    ]
-  }, null, 2));
+  res.status(200).send(JSON.stringify(
+    {"errors":[{"code":0,"message":""}]}
+, null, 2));
 });
 
 // Listen on Railway port
